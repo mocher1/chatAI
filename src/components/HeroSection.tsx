ca300adb39cb2f83acbbca89b82af220c31f41f5
mocher1 <48806962+mocher1@users.noreply.github.com>
2025-06-12 10:53:17 +0200

    fix hero heading color

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection: React.FC = () => (
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
     >
-      <h1 className="text-4xl md:text-5xl font-extrabold text-gray-9000 leading-snug tracking-tight">
+      <h1 className="text-4xl md:text-5xl font-extrabold text-gray-900 leading-snug tracking-tight">
         Masz pytanie o karierę?{' '}
         <span className="text-indigo-600">
           <Typewriter
